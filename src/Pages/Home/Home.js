ac7b3079daee96970fb4e51ff20ed37b5471bb6b
sraboni-bhuiyan <bhuiyansraboni@gmail.com>
2022-05-22 21:32:30 +0200

    footer added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Footer from './Footer';
 import Info from './Info/Info';
 import MakeAppointment from './MakeAppointment';
 import Services from './Services';
@@ -13,6 +14,7 @@ const Home = () => {
             <Services></Services>
             <MakeAppointment></MakeAppointment>
             <Testimonials></Testimonials>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -4,7 +4,7 @@ const Review = ({review}) => {
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
             <div class="card-body">
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae accusantium expedita assumenda voluptatibus amet est placeat a. Quae iure vitae, aspernatur at enim quo aliquid recusandae nam corporis possimus natus!</p>
                 <div>
                     <div className="flex items-center">
                         <div className="avatar">
