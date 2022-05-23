1ec7f3fa9d20c602b99f94b51073b1ea070e4365
sraboni-bhuiyan <bhuiyansraboni@gmail.com>
2022-05-23 18:51:16 +0200

    loginpage with social login and auth settings

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Service = ({service, setTreatment}) => {
     const {name, slots} = service;
     return (
-        <div class="card lg:max-w-lg shadow-xl">
-            <div class="card-body text-center">
-                <h2 class="text-xl font-bold text-center text-secondary">{name}</h2>
+        <div className="card lg:max-w-lg shadow-xl">
+            <div className="card-body text-center">
+                <h2 className="text-xl font-bold text-center text-secondary">{name}</h2>
                 <p>
                     {
                         slots.length > 0
@@ -14,12 +14,12 @@ const Service = ({service, setTreatment}) => {
                     }
                 </p>
                 <p>{slots.length} {slots.length > 1 ? 'slots' : 'slot'} available</p>
-                <div class="justify-center">
+                <div className="justify-center">
                 <label 
                 for="booking-modal" 
                 disabled={slots.length === 0} 
                 onClick={() => setTreatment(service)}
-                class="btn btn-secondary text-white bg-gradient-to-r from-secondary to-primary">Book Appointment</label>
+                className="btn btn-secondary text-white bg-gradient-to-r from-secondary to-primary">Book Appointment</label>
                 </div>
             </div>
         </div>
